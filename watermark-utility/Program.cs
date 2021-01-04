d571718fbd61d5602a1b0ac07fea006af0f13e0c
Brandon Haugen <bhaugen@abledocs.com>
2021-01-04 14:07:55 -0600

    Start of project

---
+++ watermark-utility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace watermark_utility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
