c4cb1cc7ee4fee2ad60e79a7fc70bbc6fd2711c2
Brandon Haugen <bhaugen@abledocs.com>
2021-01-11 12:35:39 -0600

    Add image watermark option using AbleDocs logo (#4)
    * Changed watermark text color to AbleDocs blue
    
    * Center the watermark text on the page
    
    * Added an image option for watermarking
    
    - uses an AbleDocs logo
    
    * Updated text watermark to better match logo
    
    - slightly adjusted color
    - set opacity the same
    - increase size
    
    * Updated tests to include --image option
    
    * Do not use inline images for watermarks

--- watermark-utility/Watermarker.cs
+++ watermark-utility/Watermarker.cs
@@ -1,8 +1,12 @@
 ﻿using iText.IO.Font;
 using iText.IO.Font.Constants;
+using iText.IO.Image;
 using iText.Kernel.Colors;
 using iText.Kernel.Font;
+using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
+using iText.Kernel.Pdf.Extgstate;
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
@@ -10,6 +14,7 @@ using System;
 using System.Collections;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Resources;
 
 namespace watermark_utility
 {
@@ -40,17 +45,23 @@ namespace watermark_utility
                 new Option<String>(
                     "--text",
                     description: "The text to use as a watermark."
+                ),
+                new Option<String>(
+                    "--image",
+                    description: "Should an image be added as a watermark? Accepted values: 'true' or 'false'",
+                    getDefaultValue: () => "true"
                 )
             };
 
             rootCommand.Description = "A simple utility that adds a watermark to a PDF and writes a new copy.";
 
-            rootCommand.Handler = CommandHandler.Create<String, String, String, String>((input, output, pages, text) =>
+            rootCommand.Handler = CommandHandler.Create<String, String, String, String, Boolean>((input, output, pages, text, image) =>
             {
                 Console.WriteLine($"The value for --input is : {input}");
                 Console.WriteLine($"The value for --ouptput is : {output}");
                 Console.WriteLine($"The value for --pages is : {pages}");
                 Console.WriteLine($"The value for --text is : {text}");
+                Console.WriteLine($"The value for --image is : {image}");
 
                 if (String.IsNullOrWhiteSpace(input))
                 {
@@ -83,7 +94,7 @@ namespace watermark_utility
 
                 PdfDocument document = new PdfDocument(new PdfReader(input), new PdfWriter(output));
 
-                addWatermarkToPdf(document, pages, text);
+                addWatermarkToPdf(document, pages, text, image);
 
                 document.Close();
 
@@ -99,22 +110,34 @@ namespace watermark_utility
         /// <param name="document"></param>
         /// <param name="pages"></param>
         /// <param name="watermarkTextContent"></param>
-        protected static void addWatermarkToPdf(PdfDocument document, String pages, String watermarkTextContent)
+        protected static void addWatermarkToPdf(PdfDocument document, String pages, String watermarkTextContent, Boolean addImage)
         {
             Paragraph watermarkText = new Paragraph(watermarkTextContent);
             watermarkText.SetFont(PdfFontFactory.CreateFont(FontProgramFactory.CreateFont(StandardFonts.HELVETICA)));
-            watermarkText.SetFontSize(15);
-            watermarkText.SetFontColor(ColorConstants.BLACK);
+            watermarkText.SetFontSize(20);
+            watermarkText.SetFontColor(new DeviceRgb(10, 129, 195));
+            watermarkText.SetOpacity(0.5f);
+
+            ResourceManager resourceManager = WatermarkResources.ResourceManager;
+            byte[] imageData = (byte[])resourceManager.GetObject("AbleDocs_logo");
+            ImageData watermarkImage = ImageDataFactory.Create(imageData);
 
             if (pages == "all")
                 for (int i = 1; i < document.GetNumberOfPages() + 1; i++)
+                {
                     addWatermarkToPage(document, i, watermarkText);
+
+                    if (addImage == true)
+                    {
+                        addImageWatermarkToPage(document, i, watermarkImage);
+                    }
+                }
             else
                 Console.WriteLine("Did not watermark any pages!");
         }
 
         /// <summary>
-        /// Adds the provided text to the specified page in the PdfDocument
+        /// Adds the provided text to the specified page in the PdfDocument in the center of the page
         /// </summary>
         /// <param name="document"></param>
         /// <param name="pageNumber"></param>
@@ -122,9 +145,53 @@ namespace watermark_utility
         protected static void addWatermarkToPage(PdfDocument document, int pageNumber, Paragraph text)
         {
             PdfPage page = document.GetPage(pageNumber);
+            Rectangle pageSize = page.GetPageSizeWithRotation();
             Canvas pageCanvas = new Canvas(page, page.GetPageSize()).
-                ShowTextAligned(text, 100, 100, pageNumber, TextAlignment.CENTER, VerticalAlignment.TOP, 45);
+                ShowTextAligned(
+                    text,
+                    (pageSize.GetRight() - pageSize.GetLeft()) / 2, // Center the watermark on the horizontal axis of the page
+                    (pageSize.GetTop() - pageSize.GetBottom()) / 2, // Center the watermark on the vertical axis of the page
+                    pageNumber,
+                    TextAlignment.CENTER,
+                    VerticalAlignment.TOP,
+                    0
+                );
+
             pageCanvas.Close();
+        }
+
+        /// <summary>
+        /// Adds the provided ImageData to the specified page in the PdfDocument in the center of the page as
+        /// a watermark.
+        /// </summary>
+        /// <param name="document"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="watermarkImage"></param>
+        protected static void addImageWatermarkToPage(PdfDocument document, int pageNumber, ImageData watermarkImage)
+        {
+            PdfPage page = document.GetPage(pageNumber);
+            Rectangle pageSize = page.GetPageSizeWithRotation();
+            PdfCanvas canvas = new PdfCanvas(page);
+
+            // Create a new graphics state with an opacity of 50%
+            PdfExtGState graphicsState = new PdfExtGState().SetFillOpacity(0.5f);
+            canvas.SaveState();
+            canvas.SetExtGState(graphicsState);
+
+            // Add the image to the canvas
+            canvas.AddImageWithTransformationMatrix(
+                watermarkImage,
+                watermarkImage.GetWidth(), // use the whole width of the image
+                0,
+                0,
+                watermarkImage.GetHeight(), // use the whole height of the image
+                ((pageSize.GetRight() - pageSize.GetLeft()) / 2) - (watermarkImage.GetWidth() / 2), // Center the watermark on the horizontal axis of the page
+                (pageSize.GetTop() - pageSize.GetBottom()) / 2, // Center the watermark on the vertical axis of the page
+                false
+            );
+
+            // Restore the initial graphics state
+            canvas.RestoreState();
         }
     }
 }
